984ea0a9f7ca74a2b3a572f81f9b0ea90fbd150f
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2023-11-17 16:31:21 +0800

    Update tro.sh

--- tro.sh
+++ tro.sh
@@ -36,7 +36,6 @@ useradd -g trojan -s /usr/sbin/nologin trojan
 chown -R trojan:trojan /etc/trojan
 systemctl start trojan
 systemctl enable trojan
-systemctl status trojan
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
 echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
 sysctl -p
