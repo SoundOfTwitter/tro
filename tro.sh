d01df61cbffb67fb06c9fe32811d3f790be34750
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2023-12-03 13:54:59 +0800

    Update tro.sh

--- tro.sh
+++ tro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 publicIP=$(curl -s https://api64.ipify.org?format=json | jq -r .ip)
 # 写入文件内容
-sed -i "1s/.*/cn = &quot;$publicIP&quot;/" /home/admin/ca.txt
+sed -i "1s/.*/cn = &quot;$publicIP&quot;/" >> /home/admin/ca.txt
 # echo 'cn = \"echo $publicIP\"' >> /home/admin/ca.txt
 echo 'organization = "GlobalSign RULTR"' >> /home/admin/ca.txt
 echo 'serial = 1' >> /home/admin/ca.txt
@@ -11,7 +11,7 @@ echo 'signing_key' >> /home/admin/ca.txt
 echo 'cert_signing_key' >> /home/admin/ca.txt
 echo 'crl_signing_key' >> /home/admin/ca.txt
 # 写入文件内容
-sed -i "1s/.*/cn = &quot;$publicIP&quot;/" /home/admin/server.txt
+sed -i "1s/.*/cn = &quot;$publicIP&quot;/" >> /home/admin/server.txt
 # echo 'cn = \"echo $publicIP\"' >> /home/admin/server.txt
 echo 'organization = "GlobalSign RULTR"' >> /home/admin/server.txt
 echo 'expiration_days = 3650' >> /home/admin/server.txt
