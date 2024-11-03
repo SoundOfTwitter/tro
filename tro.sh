b7e9254fb08ec66d12ff0a8854b5f260ed5350ca
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2024-11-03 22:51:07 +0800

    Update tro.sh

--- tro.sh
+++ tro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# publicIP=$(curl -s https://ifconfig.me/ip)
-read -p "请输入本机公网IP地址: " publicIP
+publicIP=$(wget -qO- ifconfig.me)
+# read -p "请输入本机公网IP地址: " publicIP
 # 写入文件内容
 # 在/home/admin/ca.txt第一行写入'cn = "'
 echo "cn = \"" > /home/admin/ca.txt
